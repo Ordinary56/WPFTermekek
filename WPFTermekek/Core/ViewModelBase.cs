1d637fc640964ab517230c815b865d1b17b87551
tbalogh695@gmail.com <tbalogh695@gmail.com>
2023-04-27 21:10:57 +0200

    CollectionView

--- WPFTermekek/Core/ViewModelBase.cs
+++ WPFTermekek/Core/ViewModelBase.cs
@@ -9,7 +9,7 @@ namespace WPFTermekek.Core
     public class ViewModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
-        public void OnPropertyChanged([CallerMemberName] string property = "")
+        protected void OnPropertyChanged([CallerMemberName] string property = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
         }
--- WPFTermekek/MVVM/ViewModel/MainViewModel.cs
+++ WPFTermekek/MVVM/ViewModel/MainViewModel.cs
@@ -4,7 +4,10 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
 using Microsoft.Win32;
 using MySql.Data.MySqlClient;
 using WPFTermekek.Core;
@@ -14,26 +17,43 @@ namespace WPFTermekek.MVVM.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
-        string _kategoria;
-        string _gyarto;
+        string _kategoria = "-- Nincs megadva --";
+        string _gyarto = "-- Nincs megadva --";
         string _nev;
+
         //Hardver adatbázisból vesszük ki az adatokat
         //VARCHAR MEZŐKNÉL A KÓDOLÁS LEGYEN UTF-16
         static string Connectionstring = "datasource=127.0.0.1;port=3306;username=root;password=;database=hardver;";
+
         MySqlConnection _connection = new(Connectionstring);
+
         ObservableCollection<Termek> _termekek;
-        ObservableCollection<Termek> Original;
+
+        ICollectionView _termekekCollection;
         public List<string>? Gyartok_Box { get; set; }
+
         public List<string> Kategoriak_Box { get; set; }
+
+        public ICollectionView TermekekCollection
+        {
+            get => _termekekCollection;
+            set
+            {
+                _termekekCollection = value;
+                OnPropertyChanged(nameof(TermekekCollection));
+            }
+        }
+
         public ObservableCollection<Termek>? Termekek
         {
             get => _termekek!;
             set
             {
                 _termekek = value!;
-                OnPropertyChanged();
+                OnPropertyChanged(nameof(Termekek));
             }
         }
+
         public string Kategoria
         {
             get => _kategoria;
@@ -59,21 +79,22 @@ namespace WPFTermekek.MVVM.ViewModel
             {
                 _nev = value;
                 OnPropertyChanged(Nev);
+                TermekekCollection.Filter += Filter;
             }
         }
 
-        public RelayCommand FilterCommand { get; }
-        public RelayCommand SaveCommand { get; }
+        public ICommand FilterCommand { get; }
+        public ICommand SaveCommand { get; }
         public MainViewModel()
         {
             _termekek = new();
-            FilterCommand = new RelayCommand(o => { FilterQuery(); }, o => { return CanFilter(); });
-            this.PropertyChanged += queryName;
-            SaveCommand = new RelayCommand(o => { Save(); });
+            FilterCommand = new RelayCommand(FilterQuery, CanFilter);
+            SaveCommand = new RelayCommand(Save);
             ConnectToDatabase();
-
+            
 
         }
+        //Meghíváskor (program bezárásakor) törli a kapcsolatot
         ~MainViewModel()
         {
             _connection.Dispose();
@@ -96,71 +117,73 @@ namespace WPFTermekek.MVVM.ViewModel
                     ));
             }
             reader.Close();
-            Kategoriak_Box = _termekek.Select(x => x.kategoria)
+            Kategoriak_Box = _termekek
+                .Select(x => x.kategoria)
                 .Distinct()
                 .ToList()!;
             Kategoriak_Box.Insert(0, "-- Nincs megadva --");
-            Gyartok_Box = _termekek.Select(x => x.gyarto)
-                .Distinct().ToList()!;
+            Gyartok_Box = _termekek
+                .Select(x => x.gyarto)
+                .Distinct()
+                .ToList()!;
             Gyartok_Box.Insert(0, "-- Nincs megadva --");
             _connection.Close();
             command.Dispose();
             reader.Dispose();
+            _termekekCollection = CollectionViewSource.GetDefaultView(Termekek);
 
-            Original = _termekek;
         }
 
-        void FilterQuery()
+        void FilterQuery(object paramter)
         {
+            TermekekCollection.GroupDescriptions.Add(new PropertyGroupDescription(Kategoria));
+            TermekekCollection.GroupDescriptions.Add(new PropertyGroupDescription(Gyarto));
 
-            var filter = new ObservableCollection<Termek>(_termekek!.Where(x => x.kategoria!.Equals(Kategoria)
-            || x.gyarto.Equals(Gyarto)));
 
-            Termekek = filter.Count() > 0 ? filter : Original;
-
-            OnPropertyChanged(nameof(Termekek));
 
         }
-        void Save()
+        void Save(object parameter)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Comma seperated values|*.csv|Text file|*.txt";
+            saveFileDialog.FileName = "Save";
+            saveFileDialog.DefaultExt = "csv";
             if ((bool)saveFileDialog.ShowDialog()!)
             {
-                using (StreamWriter sw = new($"./{saveFileDialog.FileName}"))
+                Stream fileStream = saveFileDialog.OpenFile();
+                using (StreamWriter sw = new(fileStream))
                 {
-                    foreach (Termek termek in _termekek)
+                    foreach (Termek termek in TermekekCollection)
                     {
                         sw.WriteLine(termek.ToCSVString());
                     }
                 }
             }
-            MessageBox.Show($"Sikeresen lementettük az adatokat ({_termekek.Count()} adat eltárolva)",
+            MessageBox.Show($"Sikeresen lementettük az adatokat ({TermekekCollection.Cast<object>().Count()} adat eltárolva)",
                 "SaveSucces",
                 MessageBoxButton.OK,
                 MessageBoxImage.Information);
+
         }
-        bool CanFilter()
+        bool CanFilter(object parameter)
         {
-            return !string.IsNullOrEmpty(Kategoria)
-                || !string.IsNullOrEmpty(Gyarto);
+            return !string.IsNullOrEmpty(Kategoria) &&
+                   !string.IsNullOrEmpty(Gyarto);
         }
 
-        void queryName(object sender, PropertyChangedEventArgs e)
+        bool Filter(object parameter)
         {
-            if (e.PropertyName == Nev)
+            Termek termek = parameter as Termek;
+
+            if (!string.IsNullOrEmpty(Nev))
             {
-                if (Nev == "")
-                {
-                    Termekek = Original;
-                }
-                else
-                {
-                    Termekek = new ObservableCollection<Termek>(_termekek.Where(
-                        x => x.nev!.Contains(Nev)));
-                }
+                return termek.nev.ToLower().Contains(Nev.ToLower());
 
-                OnPropertyChanged(nameof(Termekek));
             }
+
+
+            return true;
+
         }
     }
 }
